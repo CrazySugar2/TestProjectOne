70aff4e65dd218cbe5824b0a38429e1a31445b32
CrazySugar <w90826@gmail.com>
2013-08-16 13:13:24 +0800

    1

---
+++ SKY_RemoteControl/SKY_RemoteControl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SKY_RemoteControl
+//
+//  Created by 王炫殊 on 13-8-16.
+//  Copyright (c) 2013年 王炫殊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SKY_RemoteControl/SKY_RemoteControl/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SKY_RemoteControl
+//
+//  Created by 王炫殊 on 13-8-16.
+//  Copyright (c) 2013年 王炫殊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
